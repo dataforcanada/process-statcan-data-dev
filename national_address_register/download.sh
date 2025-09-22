d23018d8a64738be400db34dc29627379a9dbac5
Diego Ripley <diego@diegoripley.ca>
2025-09-22 17:06:19 +0000

    Add NAR 2025-07 and update download links

--- national_address_register/download.sh
+++ national_address_register/download.sh
@@ -2,12 +2,14 @@
 if [ ! -d "${DATA_FOLDER}/national_address_register" ]
 then
     echo "Making directory ${DATA_FOLDER}/national_address_register/"
-    mkdir -p ${DATA_FOLDER}/national_address_register/{input,extracted,output}/{2024-12,2024-06,2023,2022}
+    mkdir -p ${DATA_FOLDER}/national_address_register/{input,extracted,output}/{2025-07,2024-12,2024-06,2023,2022}
 fi
 
 INPUT_FOLDER="${DATA_FOLDER}/national_address_register/input"
 
-echo "Downloading 2024-12 vintage of national address register files"
+echo "Downloading 2025-07 vintage of national address register files"
+aria2c -x16 -i "${SCRIPT_DIR}/national_address_register/national_address_register_files_2025_07.txt" --dir=${INPUT_FOLDER}/2025-07 --auto-file-renaming=false
+echo "Downloading 2024-12 vintage of national address register files"0
 aria2c -x16 -i "${SCRIPT_DIR}/national_address_register/national_address_register_files_2024_12.txt" --dir=${INPUT_FOLDER}/2024-12 --auto-file-renaming=false
 echo "Downloading 2024-06 vintage of national address register files"
 aria2c -x16 -i "${SCRIPT_DIR}/national_address_register/national_address_register_files_2024_06.txt" --dir=${INPUT_FOLDER}/2024-06 --auto-file-renaming=false
--- national_address_register/process.sh
+++ national_address_register/process.sh
@@ -3,6 +3,15 @@
 INPUT_FOLDER="${DATA_FOLDER}/national_address_register/input"
 EXTRACTED_FOLDER="${DATA_FOLDER}/national_address_register/extracted"
 
+process_202507() {
+    # Process 2025-07 vintage
+    echo "Extracting ${INPUT_FOLDER}/2025-07/202507.zip to ${EXTRACTED_FOLDER}/2025-07"
+    unzip -q -n ${INPUT_FOLDER}/2025-07/202507.zip -d ${EXTRACTED_FOLDER}/2025-07
+    jupyter execute process_2025_07.ipynb
+    rclone --progress copy /data/national_address_register/output/2025-07/nar_2025_07.parquet cloudflare:/dataforcanada-data/processed/statistics_canada/national_address_register/2025-07/
+}
+
+
 process_202412() {
     # Process 2024-12 vintage
     echo "Extracting ${INPUT_FOLDER}/2024-12/202412.zip to ${EXTRACTED_FOLDER}/2024-12"
@@ -31,7 +40,8 @@ process_2022() {
     # Encoding is latin-1
 }
 
-process_202412
-process_202406
+process_202507
+#process_202412
+#process_202406
 #process_2023
 #process_2022
